38e6ce186ec2ad5eec7345a57c1814f99f6a4f18
Anna <Anna@Annas-MacBook-Pro.local>
2020-04-15 23:16:54 +0200

    commit routes

--- run.py
+++ run.py
@@ -1,14 +1,29 @@
-
 import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello There !</h1>"
+    """ Main page with instructions """
+    return "To send a message use: /USERNAME/MESSAGE"
 
+
+@app.route("/<username>")
+def user(username):
+    '''Display chat messages'''
+    return "Welcome {0}".format(username)
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
 
 if __name__ =="__main__":
     app.run(host=os.getenv("IP"),
