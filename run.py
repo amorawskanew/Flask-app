d8516e3555bc867c29efe6b02ce796ea4a6b77dc
Anna <Anna@Annas-MacBook-Pro.local>
2020-05-01 13:24:37 +0200

    commit

--- run.py
+++ run.py
@@ -39,6 +39,10 @@ def user(username):
                            chat_messages=messages)
 
 
-app.run(host=os.getenv("IP", "127.0.0.1"),
-        port=int(os.getenv("PORT", "5000")), debug=False)
-        
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True,
+    )
+
